972e15705f16df7a23d43129476a01a9192a13f4
C00433 <islah.musleh@tmrnd.com.my>
2024-05-12 11:49:20 +0800

    feat: add housing-location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    HousingLocationComponent
   ],
   template: `
     <section>
@@ -14,6 +16,9 @@ import { CommonModule } from '@angular/common';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
   `,
   styleUrls: ['./home.component.css'],
 })
